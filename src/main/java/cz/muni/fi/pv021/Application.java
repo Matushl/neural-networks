c9a924d82835e7280adbc9b1fd3ece645f6be078
domhanak <dhanak@redhat.com>
2016-12-07 11:24:54 +0100

    Add Application class fro main App logic

---
+++ src/main/java/cz/muni/fi/pv021/Application.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pv021;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * Created by dhanak on 12/7/16.
+ */
+public class Application {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+       // main app logic here
+
+    }
+}
